9efe965cf5bb9131e36b60a208d272e045ace31c
Lan Dao <phlandao@gmail.com>
2020-12-28 12:21:43 -0500

    contact form updated

--- contact-form.php
+++ contact-form.php
@@ -3,7 +3,7 @@
   $visitor_email = $_POST['f-email'];
   $message = $_POST['message'];
 
-  $email_from = 'phlandao@gmail.com';
+  $email_from = 'landao2607.github.io';
   $email_subject = "New Portfolio Web Form Submission";
   $email_body = "User Name: $name.\n". "User Email: $visitor_email.\n". "User Message: $message.\n";
   $to = "ldao@millikin.edu";
@@ -12,5 +12,4 @@
   mail($to,$email_subject,$email_body,$headers);
 
   header("Location: index.html");
-
  ?>
