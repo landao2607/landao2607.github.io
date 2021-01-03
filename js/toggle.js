c69dcc217bc8853602d6d0267daf8b192cc3a3eb
Lan Dao <phlandao@gmail.com>
2021-01-03 15:32:20 -0500

    fixed responsive nav bar

---
+++ js/toggle.js
@@ -0,0 +1,10 @@
+const navSlide = () => {
+	const toggle = document.querySelector('.toggle');
+
+
+	toggle.addEventListener('click'.() => {
+		//Toggle animation
+		toggle.classList.toggle('toggle');
+	});
+}
+navSlide();
